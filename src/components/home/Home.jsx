26201068f5ce244989906473a02c5bd044b03780
Tiago Damasceno <tiagodepd@yahoo.com.br>
2022-07-19 18:42:45 -0300

    Second

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,7 @@ import Main from '../template/Main'
 
 export default props =>
     <Main icon="home" title="Início"
-        subtitle="Segundo Projeto do capítulo de React.">
+        subtitle="Projeto para treinar CRUD com React.">
         <div className='display-4'>Bem Vindo!</div>
         <hr />
         <p className="mb-0">Sistema para exemplificar a construção
